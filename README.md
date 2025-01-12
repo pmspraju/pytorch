288445fed8384d7ceb7a47b8b1f998f296111df2
pmspraju <pmspraju@gmail.com>
2025-01-12 15:20:55 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+### Pytorch
+#### Learning Pytorch
+
+#### Installation
+```
+python3.10 -m venv pytorch_env
+source pytorch_env/bin/activate
+pip install pipenv
+pipenv install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+pipenv shell
+```
---
+++ init/helloworld.py
@@ -0,0 +1,9 @@
+import os.path
+import torch
+print(f"Hello, PyTorch {torch.__version__}!")
+print(f"Your current working directory is {os.getcwd()}")
+print(f"Cuda is available: {torch.cuda.is_available()}")
+if torch.cuda.is_available():
+    print(f"Cuda version: {torch.version.cuda}")
+    print(f"Cuda device count: {torch.cuda.device_count()}")
+    print(f"Cuda device name: {torch.cuda.get_device_name()}")
